d2cb9effac78099cccdaa8cab9a448937f3dec7c
Amal Hirsi <ahirsi@hotmail.co.uk>
2022-09-05 01:58:22 +0100

    First commit

--- index.js
+++ index.js
@@ -1,7 +1,56 @@
 // TODO: Include packages needed for this application
+const fs = require ("fs");
+const inquirer = require ("inquirer")
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const generatePage = require ("./utils/generateMarkdown.js");
+
+// These are the array of questions for the user.
+const questions = [
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "GitHub"
+    },
+    {
+        type: "input",
+        message: "What is your email?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is the title of your project",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Please write a short description of your project?",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "What command should be run to run tests?",
+        name: "tests"
+    },
+    {
+        type: "input",
+        message: "What command should be run for instillation?",
+        name: "instillation"
+    },
+    {
+        type: "input",
+        message: "What kind of license should your project have?",
+        name: "license",
+        choices: [
+            "MIT",
+            "APACHE",
+            "GPL",
+            "BSD",
+            "NONE"
+        ]
+
+    },
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
